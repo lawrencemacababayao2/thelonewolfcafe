36ab24a758481a322b8b116fa5f57f35c9bf8e23
lawrencemacababayao <lawrencemacababayao2>
2026-05-23 14:14:23 +0800

    pass

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/thelonewolfcafe/',
+  base: process.env.VITE_BASE_PATH || "/thelonewolfcafe",
   plugins: [
     react(),
     tailwindcss()
